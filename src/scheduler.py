647dcdf0fb307b4533d754aa4014da31c417b33d
meggavenda-dev <meggavenda@gmail.com>
2026-02-09 11:00:35 -0300

    Refactor build_slots and assert_no_conflict functions
    Remove unnecessary comments and clean up code formatting.

--- src/scheduler.py
+++ src/scheduler.py
@@ -6,19 +6,15 @@ def parse_hhmm(s: str) -> time:
     return time(int(hh), int(mm))
 
 def build_slots(date_obj, tz_name: str, work_start="08:00", work_end="18:00", slot_minutes=15, visit_minutes=45):
-    """Retorna lista de slots sugeridos: [(start_dt, end_dt), ...]"""
     zone = tz.gettz(tz_name)
     start_t = parse_hhmm(work_start)
     end_t = parse_hhmm(work_end)
-
     start_dt = datetime.combine(date_obj, start_t).replace(tzinfo=zone)
     end_limit = datetime.combine(date_obj, end_t).replace(tzinfo=zone)
-
     slots = []
     cursor = start_dt
     delta_slot = timedelta(minutes=slot_minutes)
     delta_visit = timedelta(minutes=visit_minutes)
-
     while cursor + delta_visit <= end_limit:
         slots.append((cursor, cursor + delta_visit))
         cursor += delta_slot
