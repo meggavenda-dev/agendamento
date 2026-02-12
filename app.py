4b848c17886b76e3eedaeb0f629446dd1bcdb877
meggavenda-dev <meggavenda@gmail.com>
2026-02-12 09:28:27 -0300

    Refactor authentication and profile loading

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-
 import streamlit as st
 from core.ui import load_css
 from core.auth import require_auth
+from core.supa import supabase_user
+from core.queries import get_profile
 
 st.set_page_config(page_title="PulseAgenda", layout="wide")
-load_css()
-_ = require_auth()
+uid = require_auth(); sb = supabase_user()
+prof = get_profile(sb, uid) or {}
+focus = (prof.get("theme") == "focus")
+load_css(focus_mode=focus)
 
 st.title("PulseAgenda")
-st.write("Use o menu lateral: Agora | Semana | Criar | Foco do Dia | Pomodoro | Entrada Rapida | Config | Exportar")
+st.caption("Seu ritmo. Seu foco. Sua agenda.")
